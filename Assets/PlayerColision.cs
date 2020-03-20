8dacc2f8d159023630687d2e59baa59ac2a217df
MarianR <mrunceanu@pentalog.com>
2020-03-20 23:52:21 +0200

    midway

---
+++ Assets/PlayerColision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
